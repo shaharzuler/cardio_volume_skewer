4ac5043260d49da12bbb4a6f1efc54ce1c7cf0d7
shaharzuler <shahar.zuler@gmail.com>
2024-04-10 19:02:28 +0300

    readme and adding some "_" func initials

--- README.md
+++ README.md
@@ -1,6 +1,76 @@
-# cardio_volume_skewer
+# Cardio Volume Skewer
 
-A package that creates a synthetic 4D sequence of a cardiac CT from a single 3D image.
+Cardio Volume Skewer is a Python package designed to generate synthetic 4D sequences of cardiac CT scans from a single 3D image. By implementing the method detailed in our [paper](soon_Link), this package creates a new 3D frame along with 3D optical flow ground truth information through radial, longitudinal, and circumferential deformations around the main axis of the cardiac left ventricle (LV).
+
+## Installation
+
+You can install the package via pip:
 ```
 pip install git+https://github.com/shaharzuler/cardio_volume_skewer
 ```
+
+## Sample Outputs:
+
+Main sections of generated sequence:
+
+![main sections of generated sequence](readme_data/vid_thetas_60.0_-20.0_rs_0.9_0.9_h_0.91_linear_mask_True_blur_radious_1.gif)
+
+Main sections of generated LV sequence:
+
+![main sections of generated LV mask sequence](readme_data/vid_mask_thetas_60.0_-20.0_rs_0.9_0.9_h_0.91_linear_mask_True_blur_radious_1.gif)
+
+Main sections of generated myocardioum sequence:
+
+![main sections of generated myocardioum mask sequence](readme_data/vid_extra_mask_thetas_60.0_-20.0_rs_0.9_0.9_h_0.91_linear_mask_True_blur_radious_1.gif)
+
+## Overview
+The package offers flexibility through configurable parameters including:
+
+- Magnitude of deformation for each component.
+- Number of frames composing the deformation.
+- Distribution method of the circumferential component along the main axis.
+- Application of deformation- optionally solely over the voxels contained in the LV segmentation mask.
+- Optional image scale-down for computational efficiency.
+
+## Example Usage
+
+```python
+from cardio_volume_skewer import create_skewed_sequences
+
+output_dir="path/to/outputs/folder"
+r1, r2, h, theta1, theta2 = 0.7, 0.7, 0.85, -20.0, 60.0
+
+paths = create_skewed_sequences(
+    r1s_end=r1, r2s_end=r2, theta1s_end=theta1, theta2s_end=theta2, hs_end=h,
+    output_dir=output_dir, 
+    template_3dimage_path="path/to/img_xyz_arr.npy",
+    template_mask_path="path/to/LV_mask.npy",
+    template_extra_mask_path="path/to/myocardioum_mask.npy",
+    num_frames=6,
+    zero_outside_mask=True,
+    blur_around_mask_radious=5,
+    theta_distribution_method="linear",
+    scale_down_by=1)
+```
+
+`template_3dimage_path` expects as input the frame of the systole represented by a NumPy array. `template_mask_path` expects a segmentation mask encompassing the left ventricle myocardium and blood cavity. Additionally, `template_extra_mask_path` expects a segmentation mask containing solely the myocardium.
+
+## Outputs
+
+For each frame, the output includes:
+
+- The deformed 3D frame, template mask, and extra mask (represented as NumPy arrays).
+- 3D optical flow ground truth notations.
+- Arrays representing the radial, longitudinal, and circumferential components on each voxel for error analysis. See [this](https://github.com/shaharzuler/four_d_ct_cost_unrolling/blob/main/four_d_ct_cost_unrolling/src/trainers/train_framework.py#L175) section  for example usage.
+
+In addition, the following outputs are provided for the entire sequence:
+
+- A video showing the three main sections of the generated sequence, template mask, and extra mask.
+- A text file mentioning the actual accepted circumferential component operating on the LV mask.
+- A plot for the distribution of the circumferential component along the main axis.
+
+
+
+## Details, Rational and Full Implementation
+For a comprehensive implementation of our paper, utilizing this package along with additional functionalities, please refer to [CardioSpectrum](link) implementation.
+ For a detailed explanation of the deformation process and rationale, please consult our [paper](link)
--- run.py
+++ run.py
@@ -1,42 +1,27 @@
 # TODO fix 1st frame glitch in video
 
 from cardio_volume_skewer import create_skewed_sequences
-import numpy as np
-import os
-output_dir="/home/shahar/cardio_corr/my_packages/cardio_volume_skewer_project/cardio_volume_skewer/outputs/magix_ts_30_test_scale_bug_5"
-for r in [ 0.7]:
-    for h in [0.85]:#0.8, 0.825]:
-        for theta1 in [0.0]:#-7.5, -5.0, -2.5, 0.0, 2.5, 5.0, 7.5]:
-            for theta2 in  [45.0]:#[-7.5, -5.0, -2.5, 0.0, 2.5, 5.0, 7.5]:
-                if False: # theta1==theta2:
-                    print(theta1,theta2,"skip")
-                # elif (theta1 in (0.0,-5.0,5.0) and theta2 in (0.0,-5.0,5.0)):
-                #     print("skip")
-                else:#if f"thetas_{theta1}_{theta2}_rs_{r}_{r}_h_{h}_linear_mask_True_blur_radious_10" not in os.listdir(output_dir):#else:
-                    print(r, r, theta1, theta2, h)
-                    paths = create_skewed_sequences(
-                        r1s_end=r, r2s_end=r, theta1s_end=theta1, theta2s_end=theta2, hs_end=h,
-                        output_dir=output_dir, 
-                        template_3dimage_path="/home/shahar/home/shahar/projects/CardioSpecrum_inference_project/CardioSpectrum_inference/sample_scan/output/magix_sample_dataset/28/orig/voxels/xyz_arr_raw.npy",
-                        template_mask_path="/home/shahar/home/shahar/projects/CardioSpecrum_inference_project/CardioSpectrum_inference/sample_scan/output/magix_sample_dataset/28/orig/voxels/xyz_voxels_mask_smooth.npy",
-                        template_extra_mask_path="/home/shahar/home/shahar/projects/CardioSpecrum_inference_project/CardioSpectrum_inference/sample_scan/output/magix_sample_dataset/28/orig/voxels/xyz_voxels_extra_mask_smooth.npy",
-                        num_frames=6,
-                        zero_outside_mask=True,
-                        blur_around_mask_radious=20,
-                        theta_distribution_method="linear",
-                        scale_down_by=4)
-                # else:
-                #     print("skip")
 
-                    # img_skewed = np.load(f"/home/shahar/cardio_corr/my_packages/cardio_volume_skewer_project/cardio_volume_skewer/outputs/magix_ts_30/thetas_{theta1}_{theta2}_rs_{r}_{r}_h_{h}_linear_mask_True_blur_radious_10/image_skewed_thetas_{theta1}_{theta2}_rs_{r}_{r}_h_{h}_linear_mask_True_blur_radious_10.npy")
-                    # mask_skewed = np.load(f"/home/shahar/cardio_corr/my_packages/cardio_volume_skewer_project/cardio_volume_skewer/outputs/magix_ts_30/thetas_{theta1}_{theta2}_rs_{r}_{r}_h_{h}_linear_mask_True_blur_radious_10/mask_skewed_thetas_{theta1}_{theta2}_rs_{r}_{r}_h_{h}_linear_mask_True_blur_radious_10.npy")
-                    # img_orig = np.load(f"/home/shahar/cardio_corr/my_packages/cardio_volume_skewer_project/cardio_volume_skewer/outputs/magix_ts_30/thetas_{theta1}_{theta2}_rs_{r}_{r}_h_{h}_linear_mask_True_blur_radious_10/image_orig_thetas_thetas_{theta1}_{theta2}_rs_{r}_{r}_h_{h}_linear_mask_True_blur_radious_10.npy")
-                    # mask_orig = np.load(f"/home/shahar/cardio_corr/my_packages/cardio_volume_skewer_project/cardio_volume_skewer/outputs/magix_ts_30/thetas_{theta1}_{theta2}_rs_{r}_{r}_h_{h}_linear_mask_True_blur_radious_10/mask_orig_thetas_thetas_{theta1}_{theta2}_rs_{r}_{r}_h_{h}_linear_mask_True_blur_radious_10.npy")
-                    # img_gt = np.load(f"/home/shahar/cardio_corr/outputs/magix_ts_90/synthetic_dataset102/01/orig/voxels/xyz_arr_raw.npy")
-                    # mask_gt = np.load(f"/home/shahar/cardio_corr/outputs/magix_ts_90/synthetic_dataset103/01/orig/voxels/xyz_voxels_mask_smooth.npy")
-                    # print(paths)
-                    # print("mask sum", (mask_gt^mask_skewed).sum())
-                    # print("img sum", (abs(img_gt-img_skewed)).sum())
+
+output_dir="/home/shahar/cardio_corr/my_packages/cardio_volume_skewer_project/cardio_volume_skewer/outputs/"
+
+for r in [ 0.7]:
+    for h in [0.85, 0.8, 0.825]:
+        for theta1 in [0.0, -7.5, -5.0, -2.5, 0.0, 2.5, 5.0, 7.5]:
+            for theta2 in  [45.0, -7.5, -5.0, -2.5, 0.0, 2.5, 5.0, 7.5]:                
+                print(r, theta1, theta2, h)
+                paths = create_skewed_sequences(
+                    r1s_end=r, r2s_end=r, theta1s_end=theta1, theta2s_end=theta2, hs_end=h,
+                    output_dir=output_dir, 
+                    template_3dimage_path="/home/shahar/home/shahar/projects/CardioSpecrum_inference_project/CardioSpectrum_inference/sample_scan/output/magix_sample_dataset/28/orig/voxels/xyz_arr_raw.npy",
+                    template_mask_path="/home/shahar/home/shahar/projects/CardioSpecrum_inference_project/CardioSpectrum_inference/sample_scan/output/magix_sample_dataset/28/orig/voxels/xyz_voxels_mask_smooth.npy",
+                    template_extra_mask_path="/home/shahar/home/shahar/projects/CardioSpecrum_inference_project/CardioSpectrum_inference/sample_scan/output/magix_sample_dataset/28/orig/voxels/xyz_voxels_extra_mask_smooth.npy",
+                    num_frames=6,
+                    zero_outside_mask=True,
+                    blur_around_mask_radious=20,
+                    theta_distribution_method="linear",
+                    scale_down_by=4)
+
 
 
 
